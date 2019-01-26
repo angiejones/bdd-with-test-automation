5c06daa6f0be61d1fc7073a8b9ad11873a6345f5
Angie Jones <jones.angie@gmail.com>
2019-01-25 17:20:10 -0800

    Update ServicesUtils.java

--- src/main/java/utils/ServicesUtils.java
+++ src/main/java/utils/ServicesUtils.java
@@ -4,8 +4,6 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-
-
 import static io.restassured.RestAssured.given;
 
 public class ServicesUtils {
